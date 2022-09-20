3f65363403ee6b942c6b43223081bf728b6b6355
mario <mario@DESKTOP-LRLSLCV.lan>
2022-09-20 14:34:48 -0400

    Third commit

--- Day2_App1/src/com/app/Sample.java
+++ Day2_App1/src/com/app/Sample.java
@@ -9,7 +9,7 @@ package com.app;
  * 
  */
 public class Sample {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
